2e92a7a194da998fc6e289a352f2f640c520e70c
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2019-04-12 12:15:34 +0200

    ENH: Bump remote module MinimalPathExtraction

--- Modules/Remote/MinimalPathExtraction.remote.cmake
+++ Modules/Remote/MinimalPathExtraction.remote.cmake
@@ -3,5 +3,5 @@ itk_fetch_module(MinimalPathExtraction
  "http://www.insight-journal.org/browse/publication/213
 "
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMinimalPathExtraction.git
-  GIT_TAG 6ae35b9d044ff2be22ce42776c64258be9252313
+  GIT_TAG 7b4e4f972639645001911b994f37c2fa831da06c
   )
