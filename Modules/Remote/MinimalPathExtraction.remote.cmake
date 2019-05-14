47768d88e02038cd89c9dc89208a5299b0cb5d65
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2019-05-14 10:04:33 +0200

    BUG: RB's branch relating to #61 of ITKMinimalPathExtraction

--- Modules/Remote/MinimalPathExtraction.remote.cmake
+++ Modules/Remote/MinimalPathExtraction.remote.cmake
@@ -3,5 +3,5 @@ itk_fetch_module(MinimalPathExtraction
  "http://www.insight-journal.org/browse/publication/213
 "
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMinimalPathExtraction.git
-  GIT_TAG 7b4e4f972639645001911b994f37c2fa831da06c
+  GIT_TAG 2f54421df763e8121c5a3f828e630c85769c81c0
   )
