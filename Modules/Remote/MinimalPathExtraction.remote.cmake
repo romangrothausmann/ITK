f0cd43a809d10aa23227d6fc2ab1f3afcfaec1d8
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2019-09-13 13:19:35 +0200

    BUG: RB's branch relating to #61 of ITKMinimalPathExtraction updated for:
    "automatic estimation of termination value"

--- Modules/Remote/MinimalPathExtraction.remote.cmake
+++ Modules/Remote/MinimalPathExtraction.remote.cmake
@@ -3,5 +3,5 @@ itk_fetch_module(MinimalPathExtraction
  "http://www.insight-journal.org/browse/publication/213
 "
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMinimalPathExtraction.git
-  GIT_TAG b37cf6164f9fa628133eefc315c68c724b45aeb2
+  GIT_TAG f89bc840d7fee36a6e303652e00d338fa35023b7
   )
